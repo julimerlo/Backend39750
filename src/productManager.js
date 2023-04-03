cfb855742a4ac102c50030714121c49e97a3a403
Julian Merlo <merlo_julian@hotmail.com>
2023-04-02 21:22:52 -0300

    desafio 3

--- src/productManager.js
+++ src/productManager.js
@@ -1,9 +1,9 @@
-const fs = require("fs");
+import fs from "fs";
 
 class ProductManager {
   constructor() {
     this.products = [];
-    this.path = "./db/productos.json";
+    this.path = "../db/productos.json";
   }
 
   appendProduct = async () => {
@@ -51,8 +51,9 @@ class ProductManager {
 
   getProducts = async () => {
     try {
-      const productosDb = await fs.promises.readFile(this.path, "utf-8");
-      console.log(productosDb);
+      const productsList = await fs.promises.readFile(this.path, "utf-8");
+      const products = JSON.parse(productsList);
+      return products;
     } catch (err) {
       console.log(err);
     }
@@ -63,9 +64,7 @@ class ProductManager {
       const productosDb = await fs.promises.readFile(this.path, "utf-8");
       const productoId = JSON.parse(productosDb);
       const find = productoId.find((value) => value.id === idProduct);
-      return console.log(
-        find ? find : "No se encontró un producto con el ID proporcionado"
-      );
+      return find;
     } catch (err) {
       console.log(err);
     }
@@ -99,9 +98,9 @@ class ProductManager {
   };
 }
 
-const product = new ProductManager();
-
-/* product.addProduct({
+/* const product = new ProductManager("./db/productos.json"); */
+/*
+product.addProduct({
   title: `CAFÉ COSTA RICA`,
   description: `Marcadas notas frutales y achocolatadas con una nota limon que da un balance perfecto.`,
   price: 3500,
@@ -126,11 +125,74 @@ product.addProduct({
   thumbnail: `https://www.modobarista.com/product_images/l/093/Catuai__98631_zoom.png`,
   code: `CBC`,
   stock: 900,
+});
+product.addProduct({
+  title: `CAFÉ TOSTADO BRASIL PEDRA`,
+  description: `catuai, caturra y mundo novo.`,
+  price: 4000,
+  thumbnail: `https://www.modobarista.com/product_images/n/062/1__42600_zoom.png`,
+  code: `CTBP`,
+  stock: 700,
+});
+product.addProduct({
+  title: `CAFÉ TOSTADO COLOMBIA SUPREMO`,
+  description: `Caturra y Typica.`,
+  price: 3000,
+  thumbnail: `https://www.modobarista.com/product_images/a/376/3__78745_zoom.png`,
+  code: `CTCS`,
+  stock: 1200,
+});
+product.addProduct({
+  title: `CAFÉ TOSTADO PERÚ`,
+  description: `Typica, Caturra y Bourbon.`,
+  price: 2000,
+  thumbnail: `https://www.modobarista.com/product_images/j/879/4__28964_zoom.png`,
+  code: `CTP`,
+  stock: 400,
+});
+product.addProduct({
+  title: `CAFÉ SALVADOR`,
+  description: `APANECA- LLAMATEPEC.`,
+  price: 5400,
+  thumbnail: `https://www.modobarista.com/product_images/x/868/Salvador___94185_zoom.png`,
+  code: `CS`,
+  stock: 100,
+});
+product.addProduct({
+  title: `CAFÉ TOSTADO COLOMBIA ORGÁNICO`,
+  description: `Typica y Caturra.`,
+  price: 4500,
+  thumbnail: `https://www.modobarista.com/product_images/u/882/2__48717_zoom.png`,
+  code: `CTCO`,
+  stock: 950,
+});
+product.addProduct({
+  title: `CAFÉ BRASIL TOPAZIO`,
+  description: `Topazio.`,
+  price: 1000,
+  thumbnail: `https://www.modobarista.com/product_images/p/041/Topazio___63224_zoom.png`,
+  code: `CBT`,
+  stock: 900,
+});
+product.addProduct({
+  title: `CAFÉ BRASIL OEIRAS TOSTADO - ORGÁNICO`,
+  description: `Frutado, Panela, taza limpia y acidez dulce.`,
+  price: 6300,
+  thumbnail: `https://www.modobarista.com/product_images/d/524/Oeiras__37832_zoom.png`,
+  code: `CBOTO`,
+  stock: 400,
 }); */
+/* product.addProduct({
+  title: `CAFÉ`,
+  description: `Comun.`,
+  price: 100,
+  thumbnail: `No img`,
+  code: `CC`,
+  stock: 400,
+}); */
+/* console.log(product.getProducts()); */
 
-/* console.log(product.getProductById(3)); */
-
-/* product.getProductById(2); */
+/* product.getProductById(7); */
 
 /* product.updateProduct(2, {
   title: "CAFÉ BRASIL BOURBON PREMIUM",
@@ -138,3 +200,5 @@ product.addProduct({
 });
  */
 /* product.deleteProduct(1); */
+
+export default ProductManager;
